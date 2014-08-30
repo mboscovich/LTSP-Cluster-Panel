1c35de4448171a8aa327ba80707fd43170ac9a81
Maximiliano Boscovich <maximiliano@boscovich.com.ar>
2014-08-30 20:31:27 -0300

    agreogo las 2 apps

---
+++ ControlPanel/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+# Create your models here.
+class Attribute(models.Model):
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.name
+
+    name = models.CharField('Name', max_length=200)
+    defaultValue = models.CharField('Default Value', max_length=200, null=True, blank=True)
+    description = models.CharField('Description', max_length=400, null=True, blank=True)
+
+class Group(models.Model):
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.name
+
+    name = models.CharField('Name', max_length=200)
+    parent = models.ForeignKey('self', null=True, blank=True)
+    attributes = models.ManyToManyField(Attribute,
+                                        through='GroupAttributeValue')
+
+
+class ThinClient(models.Model):
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.dnsName
+
+    dnsName = models.CharField('DNS Name', max_length=200, null=True, blank=True)
+    mac = models.CharField('MAC Address', max_length=200, null=True, blank=True)
+    group = models.ForeignKey(Group)
+    attributes = models.ManyToManyField(Attribute,
+                                        through='ThinClientAttributeValue')
+
+
+class GroupAttributeValue(models.Model):
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return "%s: %s = %s" % (self.group, self.attribute, self.value)
+    attribute = models.ForeignKey(Attribute)
+    group = models.ForeignKey(Group)
+    value = models.CharField(max_length=200, null=True, blank=True)
+
+
+class ThinClientAttributeValue(models.Model):
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return "%s: %s = %s" % (self.thinClient, self.attribute, self.value)
+    attribute = models.ForeignKey(Attribute)
+    thinClient = models.ForeignKey(ThinClient)
+    value = models.CharField(max_length=200, null=True, blank=True)

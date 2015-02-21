cc34b1742480a44299e5306261975d615d653c10
Maximiliano Boscovich <maximiliano@boscovich.com.ar>
2015-02-21 20:26:00 -0300

    typo

--- ControlPanel/urls.py
+++ ControlPanel/urls.py
@@ -4,8 +4,11 @@ from django.conf.urls import patterns, url
 from ControlPanel import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^thinclient/(\d+)/$', views.detail, name='detail'),
-    url(r'^thinclient/(\d+)/$', views.detail, name='groupIndex'),
-    url(r'^thinclient/(\d+)/$', views.detail, name='attib_detail'),
+    url(r'^$', views.thinClientsIndex.as_view(), name='thinClientsIndex'),
+    url(r'^groups/$', views.groupsIndex.as_view(), name='groupsIndex'),
+    url(r'^attibutes/$', views.attributesIndex.as_view(), name='attributesIndex'),
+    url(r'^thinclient/(\d+)/$', views.thinClientDetail, name='thinClientDetail'),
+    url(r'^group/(\d+)/$', views.groupDetail, name='groupDetail'),
+    url(r'^attribute/(\d+)/$', views.attributeDetail, name='attributeDetail'),
+
 )
--- ControlPanel/views.py
+++ ControlPanel/views.py
@@ -6,18 +6,37 @@ from django.views import generic
 # Create your views here.
 
 from ControlPanel.models import ThinClient, Group, ThinClientAttributeValue
-from ControlPanel.models import GroupAttributeValue
+from ControlPanel.models import GroupAttributeValue, Attribute
 
 
-class IndexView(generic.ListView):
-    template_name = 'ControlPanel/index.html'
+class thinClientsIndex(generic.ListView):
+    template_name = 'ControlPanel/thinClientsIndex.html'
     context_object_name = 'thinClients_list'
 
     def get_queryset(self):
         """Return all thinclients."""
         return ThinClient.objects.order_by('dnsName')
 
-def detail(request, thinclient_id):
+
+class groupsIndex(generic.ListView):
+    template_name = 'ControlPanel/groupsIndex.html'
+    context_object_name = 'groups_list'
+
+    def get_queryset(self):
+        """Return all group."""
+        return Group.objects.order_by('name')
+
+
+class attributesIndex(generic.ListView):
+    template_name = 'ControlPanel/attributesIndex.html'
+    context_object_name = 'attributes_list'
+
+    def get_queryset(self):
+        """Return all attibutes."""
+        return Attribute.objects.order_by('name')
+
+
+def thinClientDetail(request, thinclient_id):
     thinclient = get_object_or_404(ThinClient, pk=thinclient_id)
     TcAttrib = ThinClientAttributeValue.objects.filter(thinClient=thinclient.id)
     GroupAttrib = GroupAttributeValue.objects.filter(group=thinclient.group.id)
@@ -27,7 +46,7 @@ def detail(request, thinclient_id):
     for attrib in TcAttrib:
         Attributes[attrib.attribute.name]=attrib.value
 
-    return render(request, 'ControlPanel/detail.html',
+    return render(request, 'ControlPanel/thinClientDetail.html',
                     {
                         'thinclient': thinclient,
                         'TcAttrib': TcAttrib,
@@ -36,21 +55,22 @@ def detail(request, thinclient_id):
                         }
                 )
 
-def groupIndex(request, thinclient_id):
-    thinclient = get_object_or_404(ThinClient, pk=thinclient_id)
-    TcAttrib = ThinClientAttributeValue.objects.filter(thinClient=thinclient.id)
-    GroupAttrib = GroupAttributeValue.objects.filter(group=thinclient.group.id)
-    Attributes = {}
-    for attrib in GroupAttrib:
-        Attributes[attrib.attribute.name]=attrib.value
-    for attrib in TcAttrib:
-        Attributes[attrib.attribute.name]=attrib.value
+def groupDetail(request, group_id):
+    group = get_object_or_404(Group, pk=group_id)
+    GroupAttrib = GroupAttributeValue.objects.filter(group=group.id)
 
-    return render(request, 'ControlPanel/detail.html',
+    return render(request, 'ControlPanel/groupDetail.html',
                     {
-                        'thinclient': thinclient,
-                        'TcAttrib': TcAttrib,
+                        'Group': group,
                         'GroupAttrib': GroupAttrib,
-                        'Attributes': Attributes,
+                        }
+                )
+
+def attributeDetail(request, attribute_id):
+    attribute = get_object_or_404(Attribute, pk=attribute_id)
+
+    return render(request, 'ControlPanel/attributeDetail.html',
+                    {
+                        'Attribute': attribute,
                         }
                 )

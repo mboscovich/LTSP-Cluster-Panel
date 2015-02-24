2fc6de56fce9b59e08060fa21d21c6bc2f7174c3
Maximiliano Boscovich <maximiliano@boscovich.com.ar>
2015-02-24 19:41:32 -0300

    agrego redirecciones inversas a paginas de administracion para editar los items. Ahora si se pone /admin en la url, envia solo a la pagina de administración de ControlPanel.

--- LTSPClusterPanel/urls.py
+++ LTSPClusterPanel/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
+from django.core.urlresolvers import reverse_lazy
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,5 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'LTSPClusterPanel.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^ControlPanel/', include('ControlPanel.urls')),
+    url(r'^admin/$', RedirectView.as_view(url=reverse_lazy('admin:app_list',
+        args=('ControlPanel',)))),
     url(r'^admin/', include(admin.site.urls)),
 )
